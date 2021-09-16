686d1d7eba25763cf37ad4a9dd758252f70a99e1
judithshakespeare <52847756+judithshakespeare@users.noreply.github.com>
2021-09-16 17:41:48 +0900

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,105 +1,131 @@
-var generatedNum = [];
+let modal = new bootstrap.Modal(document.getElementById('exampleModal'));
+let modalBody = document.getElementById('modal-contents');
 
-// 랜덤한 세자리수 숫자를 리턴하는 함수
-function createNum () {
-    let i = 0;
-    while (i < 3) {
-        let n = Math.floor(Math.random() * 9) + 1;
-        if (! sameNum(n)) {
-            generatedNum.push(n);
-            i++;
+//랜덤한 n자리 수 숫자 배열 만들기
+let randomNum = [];
+function generateRandomNum() {
+    let i = 3;
+    while (randomNum.length < i) {
+        let n = Math.floor(Math.random() * 9 + 1);
+        if (randomNum.indexOf(n) === -1)
+            randomNum.push(n);
+    }
+}
+generateRandomNum();
+
+//input값 받아서 배열 만들기
+function generateInputNum() {
+    let inputNum = [];
+    let inputNumElem = document.getElementsByClassName("inputNum");
+    for (let i = 0; i < inputNumElem.length; ++i) {
+        let inputVal = Number(inputNumElem[i].value);
+        inputNum.push(inputVal);
+        modalBody.innerHTML = '';
+        if (inputVal === '') { //input값 비어있으면 false
+            modalBody.innerHTML += '숫자를 모두 입력해주세요.';
+            modal.show();
+            return false;
+        }
+        if (isNaN(inputVal)) { //input값 숫자 아니면 false
+            modalBody.innerHTML += '숫자만 입력 가능합니다.';
+            modal.show();
+            return false;
+        }
+        if (inputNum.length !== new Set(inputNum).size) { //input값 중복이면 false(****오류!!수정하기****)
+            modalBody.innerHTML += '중복된 숫자가 존재합니다.';
+            modal.show();
+            return false;
         }
     }
-
-    // 중복시 false를 리턴하는 함수
-    function sameNum (n) {
-        for (let i = 0; i < generatedNum.length; i++) {
-            if (n === generatedNum[i]) {
-                return true;
-            }
-        }
-        return false;
-    }
-    return generatedNum;
+    return inputNum;
 }
 
-createNum();
-console.log(generatedNum);
-
-let attempts = 0;
-function bullsnCows () {
-
-    // str을 int로 변환하여 리턴하는 함수
-    function toInt (tNum) {
-        tNum = Number(tNum);
-        return tNum;
-    }
-
-    // input값을 배열에 push
-    const inputNum = [];
-    let firstNum = document.getElementById("firstNum").value;
-    let secondNum = document.getElementById("secondNum").value;
-    let thirdNum = document.getElementById("thirdNum").value;
-    inputNum.push(toInt(firstNum), toInt(secondNum), toInt(thirdNum));
-
-    if(firstNum == '' || secondNum == '' || thirdNum == ''){ // input값이 비었는지 확인
-        alert("세자리 숫자를 모두 입력해주세요");
-        return false;
-    }
-    if (isNaN(firstNum) || isNaN(secondNum) || isNaN(thirdNum)){ // input값이 숫자인지 확인
-        alert("숫자만 입력 가능합니다");
-        return false;
-    }
-    if (firstNum==secondNum || firstNum==thirdNum || secondNum==thirdNum){ // input값이 중복인지 확인
-        alert("숫자는 중복되지 않아야 합니다");
-        return false;
-    }
-
-    attempts ++;
-
-    let strikeCount = 0; 
-    for (let i = 0; i < generatedNum.length; i++) {
-        // 배열 generatedNum의 n번째 값과 배열 inputNum의 n번째 값이 일치하면 스크라이크 카운트가 올라감
-        if (generatedNum[i] === inputNum[i]) {
-            strikeCount ++;
+//Balls and Strikes
+function ballsAndStrikes() {
+    let inputNum = generateInputNum();
+    let difference = randomNum.filter(x => inputNum.includes(x));
+    let strikeCount = 0;
+    for (let i = 0; i < randomNum.length; i++) {
+        if (randomNum[i] === inputNum[i]) {
+            strikeCount++;
         }
     }
-
-    // 배열 generatedNum과 inputNum의 교집합 구하기
-    let difference = generatedNum.filter(x => inputNum.includes(x));
-
-    let record = '';
-
-    if(difference.length > 0 && difference.length == strikeCount){ // 스크라이크만 있을 때 -S 출력
-        record = strikeCount+'S';
-    }else if(difference.length > 0 && strikeCount == 0){ // 볼만 있을 때 -B 출력
-        record = difference.length + 'B';
-    }else if(difference.length > 0 && strikeCount > 0){ // 스트라이크와 볼이 같이 있을 때 -B -S 출력
-        record = difference.length-strikeCount + 'B ' + strikeCount + 'S';
-    }else{
-        record = '';
+    if (difference.length > 0 && difference.length == strikeCount) { // 스크라이크만 있을 때 -S 출력
+        return strikeCount + 'S';
+    } else if (difference.length > 0 && strikeCount == 0) { // 볼만 있을 때 -B 출력
+        return difference.length + 'B';
+    } else if (difference.length > 0 && strikeCount > 0) { // 스트라이크와 볼이 같이 있을 때 -B -S 출력
+        return difference.length - strikeCount + 'B ' + strikeCount + 'S';
+    } else {
+        return '';
     }
+}
 
-    let result = document.getElementById('result');
-    result.innerHTML += '<div class="row"><div class="col">'+attempts+'회차</div><div class="col">'+firstNum+' '+secondNum+' '+thirdNum+'</div><div class="col">'+record+'</div></div>';
+//타이머
+let timerDisplay = document.getElementById("timerDisplay");
+let startTime, timer;
+function startTimer() {
+    startTime = new Date();
+    timer = setInterval(function () {
+        var now = new Date();
+        timerDisplay.innerHTML = ((now - startTime) / 1000).toFixed(2);
+    }, 10);
+}
+//타이머 멈추고 각 라운드 타임스탬프 배열에 push
+let timeStamp = [];
+function stopTimer() {
+    clearInterval(timer);
+    timeStamp.push(timerDisplay.innerHTML);
+}
 
-    function restart(){
-        generatedNum = [];
-        createNum();
-        result.innerHTML = '';
+let playCount = 0;
+let roundCount = 1;
+let maxRound = 3; //실행가능 라운드
+let resultWrapper = document.getElementById('result');
+
+document.getElementById('submit').onclick = function () {
+    if (generateInputNum()) { //input값에 문제가 없어야 실행
+        playCount++;
+        resultWrapper.innerHTML += '<div class="row"><div class="col">' + playCount + '회차</div><div class="col">' + generateInputNum() + '</div><div class="col">' + ballsAndStrikes() + '</div></div>';
+        if (ballsAndStrikes() == 3 + 'S') {
+            modalBody.innerHTML = '';
+            modalBody.innerHTML += 'Win! ';
+            modal.show();
+            reStart();
+        }
         document.querySelector('form').reset();
-        attempts = 0;
+        document.getElementById("focus").focus();
     }
+    if (playCount == 1) {
+        startTimer();
+    }
+    if (playCount > 9) {
+        reStart();
+        roundCount++;
+    }
+    if (roundCount > maxRound) {
+        console.log(timeStamp); // 타임스탬프 보여주기
+        timeStamp = [];
+        roundCount = 1;
+    }
+};
 
-    // 정답 맞추면 다시 시작
-    if(strikeCount == 3){
-        alert(firstNum+' '+secondNum+' '+thirdNum+' 성공!');
-        restart();
-    }
-
-    // 시도 횟수가 열 번이 넘어가면 다시 시작
-    if (attempts > 10){
-        alert('10회 안에 정답을 맞추지 못했습니다');
-        restart();
-    }
+function reStart() {
+    modalBody.innerHTML += roundCount+' 라운드가 끝났습니다.';
+    modal.show();
+    stopTimer();
+    randomNum = [];
+    generateRandomNum();
+    playCount = 0;
+    resultWrapper.innerHTML = '';
+    document.querySelector('form').reset();
+    document.getElementById("focus").focus();
 }
+
+document.querySelectorAll('.inputNum').forEach(el =>
+    el.onkeyup = e => {
+      if (e.target.value) {
+        el.nextElementSibling.focus()
+      }
+    }
+);
